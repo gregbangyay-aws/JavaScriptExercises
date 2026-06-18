0e2968f358b951c2879a9872af3b13d6c9acb4f5
gregbangyay-aws <greg.bangyay@awsys-i.com>
2026-06-18 14:37:39 +0800

    Added api.js from exercise 1

---
+++ Excercise1/api.js
@@ -0,0 +1,33 @@
+export const mockStudentDb = [
+  {
+    id: 1779353927081,
+    firstName: 'Leslie',
+    lastName: 'Andrews',
+    email: 'leslie.andrews@example.com'
+  },
+  {
+    id: 1779353927082,
+    firstName: 'Emma',
+    lastName: 'Baumgarten',
+    email: 'emma.baumgarten@example.com'
+  }
+];
+
+export const getStudents = () => {
+    return mockStudentDb;
+}
+
+export const addStudentsAPI = (newStudentData) => {
+    mockStudentDb.push(newStudentData);
+    return newStudentData;
+}
+
+console.log("Initial DB State: ", getStudents());
+addStudentsAPI({
+    id: 1,
+    firstName: 'John',
+    lastName: 'AWS',
+    email: 'john.aws@example.com'
+});
+console.log("Updated DB State: ", getStudents());
+
